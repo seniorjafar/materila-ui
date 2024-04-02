c820b1ee9ba20d2e5ca9a98334d07b548aa022d0
seniorjafar <147535212+seniorjafar@users.noreply.github.com>
2024-04-02 21:42:12 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import { useScrollToTop } from './hooks/use-scroll-to-top';
+
+import Router from './routes/sections';
+import ThemeProvider from './theme';
+
+// ----------------------------------------------------------------------
+
+export default function App() {
+  useScrollToTop();
+
+  return (
+    <ThemeProvider>
+      <Router />
+    </ThemeProvider>
+  );
+}
